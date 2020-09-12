6a31b50c51ba6c950b4cbce6a9baa8953e3a0177
Arudovwen <successahon@gmail.com>
2020-09-12 23:25:29 +0100

    update site

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,70 +1,47 @@
-var overlay = document.getElementById("overlay");
-var imagename = document.getElementById("sitename");
-var siteimage = document.getElementById("siteimage");
-var image = document.getElementsByClassName("mini_image");
-var close =document.getElementById("close");
-var main = document.getElementById('main')
+var leftarrow = document.getElementById("leftarrow");
+var rightarrow = document.getElementById("rightarrow");
 
+var words = document.getElementsByClassName("word");
+var slideIndex = 0;
 
-
-
-
-
-
-var i = 0;
-var txt = 'SUCCESS AHON - A highly motivated full stack developer, Familiar with development and deployment process for many web-based technologies.'; /* The text */
-var speed = 50; /* The speed/duration of the effect in milliseconds */
-
-function typeWriter() {
-   
-  if (i < txt.length) {
-    document.getElementById("demo").innerHTML += txt.charAt(i);
-    i++;
-    setTimeout(typeWriter, speed);
-  }
-
+for (let i = 0; i < words.length; i++) {
+  words[i].style.display = "none";
+  words[slideIndex].style.display = "block";
 }
-setTimeout(()=>{
-    document.getElementById("demo").innerHTML = ''
-    typeWriter()
-},4000)
-Array.from(image).forEach((element) => {
-  element.addEventListener("click", (el) => {
-    siteimage.src = el.srcElement.src;
 
-   
-    imagename.innerHTML =  el.srcElement.alt
-    if (el.srcElement.alt == "bizguruh website") {
-        imagename.href = 'https://bizguruh.com'
-    }
-    if (el.srcElement.alt == "Abaytour website") {
-        imagename.href = 'https://a-baytours.com'
-    }
-    if (el.srcElement.alt == "PMS website") {
-        imagename.href = 'https://arudovwen.github.io'
-    }
-    if (el.srcElement.alt == "IMS website") {
-        imagename.href = 'https://ministryofeducation.im.gov.ng'
-    }
-    if (el.srcElement.alt == "ilc") {
-        imagename.href = 'https://isl-center.herokuapp.com'
-    }
-    if (el.srcElement.alt == "Memory match website") {
-        imagename.href = 'https://brolik-pms.herokuapp.com'
-    }
-    overlay.style.display = "flex";
-    overlay.style.zIndex = 3;
-    main.style.height = '100vh'
-    main.style.overflow = 'hidden'
-  });
+rightarrow.addEventListener("click", () => {
+  if (slideIndex < words.length - 1) {
+    slideIndex++;
+    words[slideIndex - 1].style.display = "none";
+    words[slideIndex].style.display = "block";
+  } else {
+    slideIndex = 0;
+    words[0].style.display = "block";
+  }
 });
 
+leftarrow.addEventListener("click", () => {
+  if (slideIndex !== 0) {
+    slideIndex--;
+  
+    words[slideIndex].style.display = "block";
+  }
+});
 
-close.addEventListener('click',()=>{
-    overlay.style.display = "none";  
-    overlay.style.zIndex = 0;
-    main.style.height = '100%'
-    main.style.overflow = 'none'
-    document.getElementById('project').scrollIntoView()
-   
-})
+// var i = 0;
+// var txt = 'SUCCESS AHON - A highly motivated full stack developer, Familiar with development and deployment process for many web-based technologies.'; /* The text */
+// var speed = 50; /* The speed/duration of the effect in milliseconds */
+
+// function typeWriter() {
+
+//   if (i < txt.length) {
+//     document.getElementById("demo").innerHTML += txt.charAt(i);
+//     i++;
+//     setTimeout(typeWriter, speed);
+//   }
+
+// }
+// setTimeout(()=>{
+//     document.getElementById("demo").innerHTML = ''
+//     typeWriter()
+// },4000)
