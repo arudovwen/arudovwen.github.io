b0feb4f5940c83e98c50cf260fb8840f640b4d4c
Arudovwen <successahon@gmail.com>
2020-09-14 00:42:10 +0100

    update

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,22 @@ var leftarrow = document.getElementById("leftarrow");
 var rightarrow = document.getElementById("rightarrow");
 
 var words = document.getElementsByClassName("word");
+var project_background = document.getElementsByClassName("project-background");
+var project_name = document.getElementsByClassName("project-name");
+var project_desc = document.getElementsByClassName("project-desc");
+var project_stack = document.getElementsByClassName("project-stack");
+var project_contribution = document.getElementsByClassName("project-contribution");
+var opening_text = document.getElementById('opening-text')
+var first = document.getElementById('first')
+var second = document.getElementById('second')
+var demo_src = document.getElementsByClassName("demo-src");
 var slideIndex = 0;
+
+
+var abay = './assets/images/projects/project2.jpg';
+var isl = './assets/images/projects/project6.jpg';
+var bizzy = './assets/images/projects/project4.jpg';
+var mini = './assets/images/projects/project8.jpg';
 
 for (let i = 0; i < words.length; i++) {
   words[i].style.display = "none";
@@ -28,20 +43,54 @@ leftarrow.addEventListener("click", () => {
   }
 });
 
-// var i = 0;
-// var txt = 'SUCCESS AHON - A highly motivated full stack developer, Familiar with development and deployment process for many web-based technologies.'; /* The text */
-// var speed = 50; /* The speed/duration of the effect in milliseconds */
 
-// function typeWriter() {
+var i = 0;
+var j = 0;
+var k = 0;
+var txt = "Hello, I'm Success. Nice to meet you."; /* The text */
+var name = 'Success';
+var second_name = 'Arodowoghene Ahon'
+var speed = 150; /* The speed/duration of the effect in milliseconds */
 
-//   if (i < txt.length) {
-//     document.getElementById("demo").innerHTML += txt.charAt(i);
-//     i++;
-//     setTimeout(typeWriter, speed);
-//   }
+function typeWriter() {
 
-// }
-// setTimeout(()=>{
-//     document.getElementById("demo").innerHTML = ''
-//     typeWriter()
-// },4000)
+  if (i < txt.length) {
+    opening_text.innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  }
+
+
+}
+
+function lastWriter() {
+
+  if (k< second_name.length) {
+    second.innerHTML += second_name.charAt(k);
+    k++;
+    setTimeout(lastWriter, speed);
+  }
+
+
+}
+
+  
+
+function nameWriter() {
+
+
+  if (j < name.length) {
+    first.innerHTML += name.charAt(j);
+    j++;
+    if (j == name.length) {
+     lastWriter()
+    }
+    setTimeout(nameWriter, speed);
+  }
+
+}
+setTimeout(()=>{
+ 
+    typeWriter()
+},4000)
+nameWriter()
